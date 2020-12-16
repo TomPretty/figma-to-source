83ae23d7136972e3cbde5d86354a8c89415800b4
Tom Pretty <tompretty2@gmail.com>
2020-12-16 13:17:03 +0000

    Get basic generation working

--- code.js
+++ code.js
@@ -1,16 +1,55 @@
+const parsePriority = (priority) => {
+    if (priority === 'Primary') {
+        return 'primary';
+    }
+    else if (priority === 'Secondary') {
+        return 'secondary';
+    }
+    else if (priority === 'Tertiary') {
+        return 'tertiary';
+    }
+    else {
+        return 'subdued';
+    }
+};
+const NAME_REGEX = /\d\. (Primary|Secondary|Tertiary|Subdued)/;
+const parseText = (node, priority) => {
+    let frameNode;
+    if (priority === 'primary') {
+        frameNode = node.children[0];
+    }
+    else {
+        frameNode = node.children[0].children[0];
+    }
+    const textNode = frameNode.children[0];
+    return textNode.characters;
+};
+const parseNode = (node) => {
+    const name = node.name;
+    const match = name.match(NAME_REGEX);
+    const priority = parsePriority(match[1]);
+    const text = parseText(node, priority);
+    return { priority, text };
+};
+const getSourceButtonCode = (info) => {
+    return (`
+<Button
+  priority="${info.priority}"
+  size="default"
+>
+  ${info.text}
+</Button>`);
+};
+const sendCodeToUi = (text) => {
+    figma.ui.postMessage({ type: "code", code: text });
+};
 figma.showUI(__html__);
 figma.ui.onmessage = msg => {
-    if (msg.type === 'create-rectangles') {
-        const nodes = [];
-        for (let i = 0; i < msg.count; i++) {
-            const rect = figma.createRectangle();
-            rect.x = i * 150;
-            rect.fills = [{ type: 'SOLID', color: { r: 1, g: 0.5, b: 0 } }];
-            figma.currentPage.appendChild(rect);
-            nodes.push(rect);
-        }
-        figma.currentPage.selection = nodes;
-        figma.viewport.scrollAndZoomIntoView(nodes);
+    if (msg.type === "generate") {
+        const nodes = figma.currentPage.selection;
+        const node = nodes[0];
+        const info = parseNode(node);
+        const code = getSourceButtonCode(info);
+        sendCodeToUi(code);
     }
-    figma.closePlugin();
 };
--- code.ts
+++ code.ts
@@ -1,18 +1,74 @@
+type Priority = "primary" | "secondary" | "tertiary" | "subdued";
+interface SourceButtonInfo {
+  priority: Priority,
+  text: string,
+}
+
+const parsePriority = (priority: string): Priority => {
+  if (priority === 'Primary') {
+    return 'primary'
+  }
+  else if (priority === 'Secondary') {
+    return 'secondary'
+  }
+  else if (priority === 'Tertiary') {
+    return 'tertiary'
+  }
+  else {
+    return 'subdued'
+  }
+}
+
+const NAME_REGEX = /\d\. (Primary|Secondary|Tertiary|Subdued)/ 
+
+const parseText = (node: InstanceNode, priority: Priority): string => {
+  let frameNode: FrameNode;
+  if (priority === 'primary') {
+    frameNode = node.children[0] as FrameNode;
+  } else {
+    frameNode = (node.children[0] as FrameNode).children[0] as FrameNode;
+  }
+  const textNode = frameNode.children[0] as TextNode;
+
+  return textNode.characters;
+}
+
+const parseNode = (node: InstanceNode): SourceButtonInfo => {
+  const name = node.name;
+  const match = name.match(NAME_REGEX)
+  const priority = parsePriority(match[1])
+  const text = parseText(node, priority);
+
+  return { priority, text }
+}
+
+const getSourceButtonCode = (info: SourceButtonInfo): string => {
+  return (
+  `
+<Button
+  priority="${info.priority}"
+  size="default"
+>
+  ${info.text}
+</Button>`
+  )
+
+}
+
+const sendCodeToUi = (text: string): void => {
+  figma.ui.postMessage({ type: "code", code: text})
+}
+
 figma.showUI(__html__);
 
 figma.ui.onmessage = msg => {
-  if (msg.type === 'create-rectangles') {
-    const nodes: SceneNode[] = [];
-    for (let i = 0; i < msg.count; i++) {
-      const rect = figma.createRectangle();
-      rect.x = i * 150;
-      rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
-      figma.currentPage.appendChild(rect);
-      nodes.push(rect);
-    }
-    figma.currentPage.selection = nodes;
-    figma.viewport.scrollAndZoomIntoView(nodes);
-  }
+  if (msg.type === "generate") {
+    const nodes = figma.currentPage.selection;
+    const node = nodes[0] as InstanceNode;
 
-  figma.closePlugin();
+    const info = parseNode(node);
+    const code = getSourceButtonCode(info);
+
+    sendCodeToUi(code);
+  }
 };
